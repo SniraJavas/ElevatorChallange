b53474ba1b09be544db9e7623764ab4f0bebc1bd
Sinikiwe Nangoku Jumba <snirajumba@gmail.com>
2024-04-10 19:19:39 +0200

    Added the Elevator model

--- Models/Elevator.cs
+++ Models/Elevator.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ElevatorChallange.Models
 {
-    public class Elevator
+    public enum Direction
     {
+        Up,
+        Down
+    }
+
+    public class Elevator
+    { 
+        public int CurrentFloor { get; set; } // Where the lift is currently at
+        public Direction Direction { get; set; } // Indicate whether it is going up or down
+        public List<int> Queue { get; set; } // floor requests
+        public bool IsStopped { get; set; } // Indicates motion whether it stopped or not
+        public bool AreDoorsOpen { get; set; } // Indicates if the elevator doors are open
+        public bool IsEmergency { get; set; } // Indicates if the elevator is in emergency mode
+
+        public Elevator()
+        {
+            Queue = new List<int>();
+        }
     }
 }
