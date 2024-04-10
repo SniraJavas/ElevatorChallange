3747a71ddac87c399aad6c8fff5738989b8df181
Sinikiwe Nangoku Jumba <snirajumba@gmail.com>
2024-04-10 19:30:35 +0200

    Added elevator services for assigning and adding to the list.

--- Services/ElevatorService.cs
+++ Services/ElevatorService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevatorChallange.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,52 @@ using System.Threading.Tasks;
 
 namespace ElevatorChallange.Services
 {
-    internal class ElevatorService
+    public class ElevatorService 
     {
+
+        /// <summary>
+        /// Moving the elevator to the target floor
+        /// </summary>
+        /// <param name="elevator"></param>
+        /// <param name="targetFloor"></param>
+        public void MoveElevator(Elevator elevator, int targetFloor)
+        {
+            elevator.CurrentFloor = targetFloor;
+            // Might have to update direction
+        }
+
+        /// <summary>
+        /// Adding a floor request to the elevator's queue
+        /// </summary>
+        /// <param name="elevator"></param>
+        /// <param name="requestedFloor"></param>
+        public void AddRequest(Elevator elevator, int requestedFloor)
+        {
+            elevator.Queue.Add(requestedFloor);
+        }
+
+        /// <summary>
+        /// updating the state of the elevator
+        /// </summary>
+        /// <param name="elevator"></param>
+        /// <param name="currentFloor"></param>
+        /// <param name="direction"></param>
+        public void UpdateElevatorState(Elevator elevator, int currentFloor, Direction direction)
+        {
+            elevator.CurrentFloor = currentFloor;
+            elevator.Direction = direction;
+        }
+
+        
+
+        /// <summary>
+        /// Handling emergency situations
+        /// </summary>
+        /// <param name="elevator"></param>
+        public void HandleEmergency(Elevator elevator)
+        {
+            elevator.IsStopped = true;
+            elevator.AreDoorsOpen = true;
+        }
     }
 }
