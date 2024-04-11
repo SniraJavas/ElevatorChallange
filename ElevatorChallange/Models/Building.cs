2f5d3c402699708694c4077ea32705ccf33fb0fd
Sinikiwe Nangoku Jumba <snirajumba@gmail.com>
2024-04-11 10:54:03 +0200

    Initialise Building, Elevators & Floors to be on default

--- ElevatorChallange/Models/Building.cs
+++ ElevatorChallange/Models/Building.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevatorChallange.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,20 @@ namespace ElevatorChallange.Models
     {
         public List<(int Floor, Direction Direction)> Queue { get; set; } // Queue of floor requests with direction
         public List<Elevator> Elevators { get; set; } // List of elevators in the building
+        public List<Floor> Floors { get; set; } // List of floors in the building
 
         public Building()
         {
             Queue = new List<(int Floor, Direction Direction)>();
             Elevators = new List<Elevator>();
+            Floors = new List<Floor>();
+        }
+
+        public Building(List<(int Floor, Direction Direction)> Queue,List<Elevator> Elevators, List<Floor> Floors)
+        {
+            this.Queue = Queue;
+            this.Elevators = Elevators;
+            this.Floors = Floors;
         }
     }
 }
--- ElevatorChallange/Program.cs
+++ ElevatorChallange/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Initialize building, elevators, and floor service
+using ElevatorChallange.Models;
+using ElevatorChallange.Services;
+
+List<(int Floor, Direction Direction)> Queue = new List<(int, Direction)>();
+Queue.Add((1, Direction.Up));
+Queue.Add((3, Direction.Down));
+Queue.Add((5, Direction.Down));
+
+List<Floor> Floors = Enumerable.Range(1, 5) // There will be 5 floors
+                       .Select(floorNumber => new Floor(floorNumber))
+                       .ToList<Floor>();
+
+List<Elevator> Elevators = Enumerable.Range(1, 3)  // There will be 3 Elevators
+                          .Select(id => new Elevator
+                          {
+                              Id = id,
+                              CurrentFloor = 1,
+                              Direction = Direction.Up,
+                              Queue = new List<int>(),
+                              IsStopped = true,
+                              AreDoorsOpen = false,
+                              IsEmergency = false
+                          })
+                          .ToList<Elevator>();
+
+Building building = new Building(Queue, Elevators, Floors);
+
+ElevatorService elevatorService = new ElevatorService();
+FloorService floorService = new FloorService();
+
+Console.WriteLine("Welcome to the DVT Elevator ");
